de05a9fac21446ce78daafc8ace08718e919ed20
Aditya Bavadekar <bvkraditya@gmail.com>
2024-12-10 17:15:21 +0530

    Added Classification model

--- app.py
+++ app.py
@@ -1,63 +1,131 @@
 import time
 import threading
 import cv2
-from PIL import Image, ImageTk 
-from tkinter import Label, Button, Tk, PhotoImage
-        
+from PIL import Image, ImageTk
+from tkinter import Label, Button, Tk, filedialog
+from model import ClassificationModel
+
 
 class CropImageClassificationApp:
-    def __init__(self, window):
+    def __init__(self, window, fn_predict):
+        self.fn_predict = fn_predict
+        self.save_image_path = "image.jpg"
+        self.eval_image_path = lambda: self.save_image_path
+
         self.window = window
-        self.window.title("Crop Image Detection cum Classification")
-        self.window.geometry("500x400")
+        self.window.title("Crop Image Classification")
+        self.window.geometry("700x700")
+        self.window.resizable(0, 0)
+
+        self.render_capture_layout()
+
+    def render_capture_layout(self):
         self.window.configure(bg="#0000ff")
-        self.window.resizable(1, 1)
-        Label(self.window, width=400, height = 30,  bg="black").place(x=0, y =320)
-        self.capture_image_button = Button(self.window, width = 20, text = "Capture", font = ("Arial", 15),bg = "#ff0000", command=self.capture_picture)
-        self.ImageLabel = Label(self.window, width = 500, height= 320, bg = "#4682B4")
-        self.ImageLabel.place(x=0, y=0)
-        self.capture_image_button.place(x = 150, y = 360)
-        
         self.take_picture = False
         self.picture_taken = False
+
+        for widget in self.window.winfo_children():
+            widget.place_forget()
+
+        file_chooser_btn = Button(self.window, width=20, text="Select image", font=(
+            "Arial", 15), bg="#ff0000", command=self.open_file)
+        file_chooser_btn.place(x=10, y=560)
+        self.capture_image_button = Button(self.window, width=20, text="Capture", font=(
+            "Arial", 15), bg="#ff0000", command=self.capture_picture)
+        self.ImageLabel = Label(self.window, bg="white")
+        self.ImageLabel.place(x=0, y=0)
+        self.capture_image_button.place(x=360, y=560)
         self.app_main()
+
+    def open_file(self):
+        file_path = filedialog.askopenfilename(
+            title="Select a File",
+            filetypes=(
+                ("JPEG Files", "*.jpeg"),
+                ("JPG Files", "*.jpg"),
+                ("PNG Files", "*.png"),
+            )
+        )
+        if file_path:
+            print(f"Selected File: {file_path}")
+            self.eval_image_path = lambda: file_path
+            self.picture_taken = True
 
     def capture_picture(self):
         if not self.picture_taken:
             print('Taking a Picture')
             self.take_picture = True
             print("Saving the Picture")
-            cv2.imwrite("image.jpg", self.last_frame)
+            cv2.imwrite(self.save_image_path, self.last_frame)
             self.take_picture = False
             self.picture_taken = True
-            exit(-1)
+            self.eval_image_path = lambda: self.save_image_path
+            print("Picture Saved")
 
- 
+    def change_to_results_layout(self, predicted_class, prediction_confidence):
+        for widget in self.window.winfo_children():
+            widget.place_forget()
+
+        text_color = 'black'
+        text_bg = 'green'
+        font = ("Monospace", 20)
+        self.window.configure(bg="green")
+
+        self.ImageLabel = Label(self.window, bg="white")
+        self.ImageLabel.place(x=20, y=300)
+        Label(self.window, text="Image Classification Results", font=(
+            "Arial", 30, "bold"), bg=text_bg).place(x=50, y=30)
+        Label(self.window, text=f"Predicted Class: '{
+              predicted_class.upper()}'", font=font, bg=text_bg).place(x=150, y=100)
+        Label(self.window, text=f"Prediction Confidence: {
+              prediction_confidence:.2f}%", font=font, bg=text_bg).place(x=150, y=150)
+        Button(self.window, text="Re-capture",
+               bg="#ff0000", command=self.render_capture_layout).place(x=100, y=250)
+        Button(self.window, text="Exit",
+               bg="#ff0000", command=self.window.quit).place(x=350, y=250)
+
+        frame = cv2.imread(self.eval_image_path())
+        picture = Image.fromarray(frame)
+        picture = picture.resize((300, 300), resample=0)
+        picture = ImageTk.PhotoImage(picture)
+        self.ImageLabel.configure(image=picture)
+        self.ImageLabel.photo = picture
+        self.window.update()
+
     def app_main(self):
         self.render_thread = threading.Thread(target=self.StartCamera)
         self.render_thread.daemon = True
         self.render_thread.start()
 
-
     def StartCamera(self):
         self.camera = cv2.VideoCapture(0)
         self.last_frame = None
         while True:
+            if self.picture_taken:
+                self.camera.release()
+                print("Predicting the Image")
+                predicted_class, prediction_confidence = self.fn_predict(
+                    self.eval_image_path())
+                print(f"Predicted Class: {predicted_class.upper()}, Prediction Confidence: {
+                      prediction_confidence:.2f}")
+                self.change_to_results_layout(
+                    predicted_class, prediction_confidence)
+                break
+
             ret, frame = self.camera.read()
             if ret and not self.take_picture:
                 self.last_frame = frame
                 picture = Image.fromarray(frame)
                 picture = picture.resize((500, 400), resample=0)
                 picture = ImageTk.PhotoImage(picture)
-                self.ImageLabel.configure(image = picture)
+                self.ImageLabel.configure(image=picture)
                 self.ImageLabel.photo = picture
                 self.window.update()
                 time.sleep(0.001)
-            else:
-                print("Your camera stopped")
-                break
-            
 
-root = Tk()
-App = CropImageClassificationApp(root)
-root.mainloop()
+
+if __name__ == "__main__":
+    root = Tk()
+    model = ClassificationModel()
+    App = CropImageClassificationApp(root, model.predict)
+    root.mainloop()
--- model.py
+++ model.py
@@ -3,11 +3,13 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
-print(tf.version.VERSION)
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Downalod the model and save to saved_models/ directory.
+https://drive.google.com/file/d/1-x4UlKuHSOj5KjrRdSglWi9T4cmmrj5m/view?usp=drive_link
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
 
-model = tf.keras.models.load_model('saved_models/entire_model_2024_12_02_195243.855337.keras')
-
-img_height, img_width = 180, 180
 class_names = """apple
 avocado
 bamboo
@@ -78,26 +80,21 @@ turnip
 watermelon
 wheat""".split('\n')
 
-# model.summary()
 
-def pred(crop_img_url:str):
-  crop_img_path = tf.keras.utils.get_file(origin=crop_img_url)
-
-  img = tf.keras.utils.load_img(
-      crop_img_path, target_size=(img_height, img_width)
-  )
-  img_array = tf.keras.utils.img_to_array(img)
-  img_array = tf.expand_dims(img_array, 0) # Create a batch
-
-  predictions = model.predict(img_array)
-  score = tf.nn.softmax(predictions[0])
-
-  print(
-      "This image most likely belongs to {} with a {:.2f} percent confidence."
-      .format(class_names[np.argmax(score)], 100 * np.max(score))
-  )
+class ClassificationModel:
   
-# Cotton Image
-image_url = 'https://www.cottonusa.org/uploads/images/cottonball.jpg'
+  def __init__(self):
+    self.model = tf.keras.models.load_model('saved_models/entire_model_2024_12_02_195243.855337.keras')
+    self.img_height, self.img_width = 180, 180
 
-pred(image_url)
+  def predict(self, image_path: str):
+    img = tf.keras.utils.load_img(image_path, target_size=(self.mg_height, self.img_width))
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+    predictions = self.model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+    predicted_class = str(class_names[np.argmax(score)])
+    prediction_confidence = 100 * np.max(score)
+
+    return (predicted_class, prediction_confidence)
